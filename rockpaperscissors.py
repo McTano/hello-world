e9f79b86fab4506c07d5d4ea0b82ad3b7ae4aa5e
McTano <tsmackinlay@gmail.com>
2016-02-24 15:15:14 -0800

    Rock Paper Scissors simulator
    This is a simple rock paper scissors simulator I made as an exercise.

---
+++ rockpaperscissors.py
@@ -0,0 +1,59 @@
+import random
+import sys
+
+player_score = 0
+computer_score = 0
+
+def who_wins(player_score, computer_score):
+	player_move = raw_input("your move, mister bond...\n > ")
+	computer_move = random.choice(["rock", "paper", "scissors"])
+	
+	def show_moves(p_move, c_move):
+		print "\tyou played %s" % p_move
+		print "\tyour opponent played %s" % c_move
+	
+	if player_move == "quit" or "exit":
+		sys.exit()
+	
+	elif player_move == computer_move:
+		show_moves(player_move, computer_move)
+		print "\tIt's a tie."
+		
+	elif (player_move == "rock") and (computer_move == "paper"):
+		show_moves(player_move, computer_move)
+		print "\tpaper covers rock. you lose."
+		computer_score = computer_score + 1
+		
+	elif (player_move == "rock") and (computer_move == "scissors"):
+		show_moves(player_move, computer_move)
+		print "\trock breaks scissors. you win."
+		player_score = player_score + 1
+		
+	elif (player_move == "paper") and (computer_move == "rock"):
+		show_moves(player_move, computer_move)
+		print "\tpaper covers rock. you win,"
+		player_score = player_score + 1
+		
+	elif (player_move == "paper") and (computer_move == "scissors"):
+		print "\tscissors cut paper, you lose."
+		computer_score = computer_score + 1
+		
+	elif (player_move == "scissors") and (computer_move == "rock"):
+		show_moves(player_move, computer_move)
+		print "\trock breaks scissors. you lose."
+		computer_score = computer_score + 1
+		
+	elif (player_move == "scissors") and (computer_move == "paper"):
+		show_moves(player_move, computer_move)
+		print "\tscissors cut paper. you win."
+		player_score = player_score + 1
+	
+	else:
+		print "That is not a valid move. You lose a point."
+		player_score = player_score - 1
+
+	print "\tplayer score = %s \n\tcomputer_score = %s" % (player_score, computer_score)	
+		
+	who_wins(player_score, computer_score)
+
+who_wins(0, 0)
