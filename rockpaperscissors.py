4572ec52f42da71f6d222248ea79c65261a98128
tristan <tsmackinlay@gmail.com>
2016-05-13 18:10:06 -0700

    * moved repeated command above decision tree.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -8,26 +8,23 @@ def show_moves(p_move, c_move):
 player_move = raw_input("your move, mister bond...\n > ")
 computer_move = random.choice(["rock", "paper", "scissors"])
 
+show_moves(player_move, computer_move)
 	
 	if player_move == "quit" or "exit":
   	sys.exit()
 	
 	elif player_move == computer_move:
-		show_moves(player_move, computer_move)
 		print "\tIt's a tie."
 		
 	elif (player_move == "rock") and (computer_move == "paper"):
-		show_moves(player_move, computer_move)
 		print "\tpaper covers rock. you lose."
 		computer_score = computer_score + 1
 		
 	elif (player_move == "rock") and (computer_move == "scissors"):
-		show_moves(player_move, computer_move)
 		print "\trock breaks scissors. you win."
 		player_score = player_score + 1
 		
 	elif (player_move == "paper") and (computer_move == "rock"):
-		show_moves(player_move, computer_move)
 		print "\tpaper covers rock. you win,"
 		player_score = player_score + 1
 		
@@ -36,12 +33,10 @@ computer_move = random.choice(["rock", "paper", "scissors"])
 		computer_score = computer_score + 1
 		
 	elif (player_move == "scissors") and (computer_move == "rock"):
-		show_moves(player_move, computer_move)
 		print "\trock breaks scissors. you lose."
 		computer_score = computer_score + 1
 		
 	elif (player_move == "scissors") and (computer_move == "paper"):
-		show_moves(player_move, computer_move)
 		print "\tscissors cut paper. you win."
 		player_score = player_score + 1
 	
